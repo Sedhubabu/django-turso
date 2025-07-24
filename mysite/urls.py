c77f7a1c0ef412cc12f28e8f05a8af99b373212e
“Sedhubabu” <sedhubabu6@gmail.com>
2025-07-24 20:48:53 +0530

    update 6\

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,16 @@
 # mysite/urls.py
 
 from django.urls import path
+from django.contrib import admin
 from app.views import items, add_user, list_users
 from django.http import HttpResponse
+
+
 def home(request):
     return HttpResponse("Hello from Railway!")
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('items/', items),
     path('',home),
     path('users/add/', add_user),
