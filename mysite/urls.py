2c893da79191017f94e27c5a58e6f62092513d9c
“Sedhubabu” <sedhubabu6@gmail.com>
2025-07-24 20:37:11 +0530

    update 5

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,9 +2,13 @@
 
 from django.urls import path
 from app.views import items, add_user, list_users
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("Hello from Railway!")
 
 urlpatterns = [
     path('items/', items),
+    path('',home),
     path('users/add/', add_user),
     path('users/', list_users),
 ]
