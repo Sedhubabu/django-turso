15a9552f75fdd5174f0b9f4899cbe0cae2519fe1
“Sedhubabu” <sedhubabu6@gmail.com>
2025-07-24 21:32:13 +0530

    update 13

--- app/db.py
+++ app/db.py
@@ -4,6 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 # Example SQLite connection (change for production)
-engine = create_engine('sqlite:///embedded.db', connect_args={"check_same_thread": False})
+engine = create_engine('sqlite:///django-database.db', connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
